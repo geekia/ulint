be80de973c3bbab178f660c3f8323f477f27378f
abert <1248824453@qq.com>
2022-05-17 10:33:43 +0800

    feat(): add commitlint config

--- packages/commitlint-config-factory/lib/index.js
+++ packages/commitlint-config-factory/lib/index.js
@@ -1,7 +1,26 @@
 'use strict';
 
-module.exports = commitlintConfigFactory;
-
-function commitlintConfigFactory() {
-    // TODO
-}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'WIP',
+        'feat',
+        'fix',
+        'style',
+        'revert',
+        'docs',
+        'chore',
+        'build',
+        'perf',
+        'refactor',
+        'test',
+        'ci',
+      ],
+    ],
+    'type-case': [2, 'always', ['lower-case', 'upper-case']],
+  }
+};
