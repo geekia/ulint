e9929ac5ca67229f7a44fe595f48fbfe24fa277e
abert <1248824453@qq.com>
2022-05-17 16:54:01 +0800

    add babel-config

--- packages/babel-preset-factory/lib/index.js
+++ packages/babel-preset-factory/lib/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = {
-    
+const presetEnvOptions = {
+  "modules": false
 }
+
+// TODO：babel 预设参数和插件的执行顺序 
+
+module.exports = {
+  preset: [
+    require("@babel/preset-react"),
+    require("@babel/preset-env", presetEnvOptions),
+    require("@babel/preset-typescript")
+  ],
+  plugin: [
+    require("@babel/plugin-proposal-class-properties",{ loose: true })
+  ]
+};
